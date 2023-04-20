97508edc41dc197fe4276c8619e96a572739db7b
Karimijacky <jackiekarimi96@gmail.com>
2023-04-20 16:09:51 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - Program that prints line using printf
  * Return:0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
