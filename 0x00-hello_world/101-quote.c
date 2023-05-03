15b4456ec33573ac720b9cfbc696a2d9b92da009
Karimijacky <jackiekarimi96@gmail.com>
2023-05-02 18:01:27 -0700

    task8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program that prints a line
+ * Return: 1(success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
